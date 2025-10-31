92f6a772612fa7c8055ddb25852d2a6022b03038
Emma Griffin <emma.audrey.g@gmail.com>
2025-10-30 23:09:13 -0600

    Finish calculator

--- chapter4/Calculator.hs
+++ chapter4/Calculator.hs
@@ -1,6 +1,7 @@
 module Calculator where
 
 import Text.Read (readEither)
+import GHC.StgToJS.Types (StaticLit(BoolLit))
 
 data Expr = Lit Int
   | Add Expr Expr
@@ -20,6 +21,25 @@ eval expr =
       eval' :: (Int -> Int -> Int) -> Expr -> Expr -> Int
       eval' operator arg1 arg2 =
         operator (eval arg1) (eval arg2)
+
+safeEval :: Expr -> Either String Int
+safeEval expr =
+  case expr of
+    Lit num       -> Right num
+    Add arg1 arg2 -> eval' (+) arg1 arg2
+    Sub arg1 arg2 -> eval' (-) arg1 arg2
+    Mul arg1 arg2 -> eval' (*) arg1 arg2
+    Div arg1 arg2 -> case arg2 of
+                       Lit num -> if num == 0
+                                  then Left "Error: division by zero"
+                                  else eval' div arg1 arg2
+                       _       -> eval' div arg1 arg2
+    where
+      eval' :: (Int -> Int -> Int) -> Expr -> Expr -> Either String Int
+      eval' operator arg1 arg2 = case (safeEval arg1, safeEval arg2) of
+        (Left s, _)         -> Left s
+        (_, Left s')        -> Left s'
+        (Right i, Right i') -> Right $ operator i i'
 
 parse :: String -> Either String Expr
 parse str =
@@ -61,3 +81,29 @@ run expr =
     Right expr' ->
       let answer = show $ eval expr'
       in "The answer is: " <> answer
+
+prettyPrint :: Expr -> String
+prettyPrint expr = do
+  let rhs = eval expr
+      lhs = prettyPrint' expr
+  lhs <> " = " <> show rhs
+  where
+    prettyPrint' :: Expr -> String
+    prettyPrint' expr' = case expr' of
+        Lit num       -> show num
+        Add arg1 arg2 -> print' "+" arg1 arg2
+        Sub arg1 arg2 -> print' "-" arg1 arg2
+        Mul arg1 arg2 -> print' "×" arg1 arg2
+        Div arg1 arg2 -> print' "÷" arg1 arg2
+        where
+          needsParens :: Expr -> Expr -> Bool
+          needsParens _e1 e2 = case e2 of
+            Lit n2 -> False
+            _      -> True
+
+          print' :: String -> Expr -> Expr -> String
+          print' operator arg1 arg2 = do
+            let s = prettyPrint' arg1 <> " " <> operator <> " "
+            if needsParens arg1 arg2
+            then s <> "( " <> prettyPrint' arg2 <> " )"
+            else s <> prettyPrint' arg2
